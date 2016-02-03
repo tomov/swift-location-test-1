44c842b1fa396c3a0c994e3e1ada3689e64bab0c
Momchil Tomov <momchil@memsql.com>
2016-02-02 21:12:30 -0800

    location stuff too

--- MotionSurvey1/ViewController.swift
+++ MotionSurvey1/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import CoreMotion
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     // Instance variables
 
@@ -53,8 +54,14 @@ class ViewController: UIViewController {
         maxAccZ?.text = "\(currentMaxAccelZ).2f"
     }
     
+    let locMgr = CLLocationManager()
+    
     override func viewDidLoad() {
 
+        //
+        // Accelerometer stuff
+        //
+        
         self.resetMaxValues()
         
         // Set motion manager properties
@@ -64,6 +71,8 @@ class ViewController: UIViewController {
         print("woooot")
         
         // Start recording data
+        // TODO UNCOMMENT FOR ACCELEROMETER BADASSERY
+        /*
         motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue())
             { (data: CMAccelerometerData?, error: NSError?) -> Void
             in
@@ -72,10 +81,27 @@ class ViewController: UIViewController {
                 print("\(error)")
             }
         }
+        */
         
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //
+        // Location stuff
+        //
+        
+        locMgr.desiredAccuracy = kCLLocationAccuracyBest // TODO really?
+        locMgr.requestWhenInUseAuthorization()
+        locMgr.startUpdatingLocation()
+        locMgr.delegate = self // important
+        
     }
+    
+    
+    @IBOutlet weak var myLat: UILabel!
+    @IBOutlet weak var myLon: UILabel!
+    @IBOutlet weak var myCourse: UILabel!
+    @IBOutlet weak var mySpeed: UILabel!
+    @IBOutlet weak var myAddress: UILabel!
     
     func outputAccelerationData(acceleration: CMAcceleration) {
         print("accelerate bitch! x = \(acceleration.x) y = \(acceleration.y) z = \(acceleration.z)")
@@ -83,12 +109,9 @@ class ViewController: UIViewController {
         if fabs(acceleration.x) > fabs(currentMaxAccelX) {
             currentMaxAccelX = acceleration.x
         }
-
-        
         if fabs(acceleration.y) > fabs(currentMaxAccelY) {
             currentMaxAccelY = acceleration.y
         }
-
         if fabs(acceleration.z) > fabs(currentMaxAccelZ) {
             currentMaxAccelZ = acceleration.z
         }
@@ -101,6 +124,25 @@ class ViewController: UIViewController {
             self.updateLabels()
         }
 
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let myCurrentLoc = locations[locations.count - 1]
+        myLat.text = "\(myCurrentLoc.coordinate.latitude)"
+        myLon.text = "\(myCurrentLoc.coordinate.longitude)"
+        myCourse.text = "\(myCurrentLoc.course)"
+        mySpeed.text = "\(myCurrentLoc.speed)"
+        
+        CLGeocoder().reverseGeocodeLocation(myCurrentLoc) { (myPlacements, error) -> Void in
+            if error != nil {
+                print("\(error)")
+            }
+            
+            if let myPlacemet = myPlacements?.first
+            {
+                self.myAddress.text = "\(myPlacemet.locality!) \(myPlacemet.country!) \(myPlacemet.postalCode!)"
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
